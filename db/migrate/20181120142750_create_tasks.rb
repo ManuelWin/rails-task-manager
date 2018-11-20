ea61e0b68894a59a7184ac71ffafeb09dec3130d
Manuel Winzler <manu_elco@hotmail.de>
2018-11-20 18:01:52 -0200

    list, add and create working

--- db/migrate/20181120142750_create_tasks.rb
+++ db/migrate/20181120142750_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.integer :details
+      t.text :details
       t.boolean :completed, default: false
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2018_11_20_142750) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.integer "details"
+    t.text "details"
     t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
